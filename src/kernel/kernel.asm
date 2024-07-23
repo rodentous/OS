b8239257c519b0d07dbad934aa7b453190c7bffa
rodentous <therodentous@gmail.com>
2024-07-23 22:23:09 +0300

    vga text mode functions

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -22,8 +22,8 @@ VGA_WHITE equ 15
 
 global main
 main:
-	mov byte [color.foreground], 15
-	mov byte [color.background], 0
+	mov byte [color.foreground], VGA_WHITE
+	mov byte [color.background], VGA_BLACK
 	mov esi, string
 	
 	call write
@@ -31,32 +31,53 @@ main:
 	jmp $
 
 
+; return cx: VGA buffer id
+get_vga_buffer_id:
+	push dx
+	xor cx, cx              ; reset cx ??????
+	
+	mov cl, [cursor.column] ; load column in cl ??????
+	mov dl, [ cursor.line ]
+	
+	.multiply:              ; multiply lines by VGA width
+		cmp dl, 0
+		jle .return
+		
+		add cx, VGA_WIDTH   ; add line to cx ??????
+
+		dec dl
+		jmp .multiply
+		
+	.return:
+		shl cx, 1           ; multiply by two since each entry takes two bytes
+
+		pop dx
+		ret
+
+
 ; al: ASCII character
 set_character_at:
 	pusha
-	; color
+
+	; VGA entry is represented as 16-bit word:
+	; character byte: [ code point x8 ]
+	; attribute byte: [ foreground x4, background x3, blink ]
+
 	mov bl, [color.background]
 	mov bh, [color.foreground]
-
+	; or  bl, 0b10000000 ; this was supposed to set blink charcter (doesn't work)
 	shl bl, 4
 	or  bl, bh
 
-	; position
-	mov cx, 0
-	mov cl, [cursor.column]
-	mov dl, [cursor.line]
-	.loop:
-		cmp dl, 0
-		jle .stop
-		add cx, VGA_WIDTH
-		dec dl
-		jmp .loop
-	.stop:
-	shl cx, 1
+	mov ah, bl
 
-	; set
-	mov byte [0xB8000 + ecx], al
-	mov byte [0xB8001 + ecx], bl
+	call get_vga_buffer_id
+
+	; set entry in VGA buffer:
+	; al: character byte
+	; ah: attribute byte
+	; ecx: buffer id
+	mov word [0xB8000 + ecx], ax
 
 	popa
 	ret
@@ -67,7 +88,7 @@ write_character:
 	push cx
 	
 	mov cx, [cursor]
-	cmp al, 0x10
+	cmp al, 0x10     ; new line on line feed character
 	je .new_line
 
 	call set_character_at
@@ -79,8 +100,8 @@ write_character:
 	jmp .return
 	
 	.new_line:
-		mov cl, 0
-		inc ch
+		mov cl, 0    ; reset column
+		inc ch       ; next line
 		jmp .return
 	
 	.return:
@@ -91,44 +112,17 @@ write_character:
 
 
 ; esi: string pointer
-; return dx: length
-get_string_length:
-	push ax
-	push esi
-	
-	mov dx, 0
-
-	.loop:
-		mov al, [esi] ; load character
-		cmp al, 0 ; null character found
-		je  .return
-
-		inc esi ; next character
-		inc dx ; increment length
-
-		jmp .loop
-
-	.return:
-		pop esi
-		pop ax
-		ret
-
-
-; esi: string pointer
 write:
 	pusha
-	
-	call get_string_length
 
 	.loop:
 		mov al, [esi] ; load character
-		call write_character
-		
-		dec dx
-		cmp dx, 0 ; out of length
-		jle .return
+		cmp al, 0     ; terminate on null character
+		je  .return
 
-		inc esi ; next character
+		call write_character
+
+		inc esi       ; next character
 		jmp .loop
 
 	.return:
